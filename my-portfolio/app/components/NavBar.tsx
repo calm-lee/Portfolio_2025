21b39b0954348f3843de246149fab079a18e3ff0
Calm Lee <calmlee@Calmui-MacBookAir.local>
2025-02-28 22:03:53 +0900

    feat: add logo for theme and menu

--- my-portfolio/app/components/NavBar.tsx
+++ my-portfolio/app/components/NavBar.tsx
@@ -5,7 +5,7 @@ import {assets} from "@/assets/assets";
 const NavBar = () => {
     return (
         <>
-            <div>
+            <div className={'fixed top-0 right-0 w-[90%] -z-10 translate-y-[-80%]'}>
                <Image src={assets.header_bg_color} alt='header-bg-color' />
             </div>
          <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-between z-50">
@@ -19,8 +19,14 @@ const NavBar = () => {
                  <li><a href="#work" className='font-Ovo'>My Work</a></li>
                  <li><a href="#contact" className='font-Ovo'>Contact me</a></li>
              </ul>
-             <div>
+             <div className={'flex items-center gap-4'}>
+                 <button>
+                     <Image src={assets.moon_icon} alt={'theme_logo'} className={'w-6'}/>
+                 </button>
                  <a href={'#contact'} className="font-Ovo hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-200 rounded-full ml-4">Contact <Image src={assets.arrow_icon} alt={'contact_icon'} className="w-3"/></a>
+                 <button className={'block ml-3'}>
+                     <Image src={assets.menu_black} alt={'menu_black'} className={'md:hidden w-6'}/>
+                 </button>
              </div>
          </nav>
         </>
