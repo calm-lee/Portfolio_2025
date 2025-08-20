41b9a3195063057748b539904029fe9209726a76
calm-lee <calm_lee@naver.com>
2025-08-20 13:59:46 +0100

    refactor: deleted unused codes and changed the color of mobile menu

--- my-portfolio/app/components/NavBar.tsx
+++ my-portfolio/app/components/NavBar.tsx
@@ -25,10 +25,8 @@ const NavBar = ({
   };
 
   const closeMenu = () => {
-    if (slideMenuRef != null) {
-      if (slideMenuRef.current) {
-        slideMenuRef.current.style.transform = "translateX(16rem)";
-      }
+    if (slideMenuRef.current) {
+      slideMenuRef.current.style.transform = "translateX(16rem)";
     }
   };
 
@@ -96,17 +94,6 @@ const NavBar = ({
               aria-hidden={true}
             />
           </button>
-          {/*<a*/}
-          {/*  href={"#contact"}*/}
-          {/*  className="font-Ovo hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-200 rounded-full ml-4 dark:border-white/50"*/}
-          {/*>*/}
-          {/*  Contact{" "}*/}
-          {/*  <Image*/}
-          {/*    src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon}*/}
-          {/*    alt={"contact_icon"}*/}
-          {/*    className="w-3"*/}
-          {/*  />*/}
-          {/*</a>*/}
           <button className={"block md:hidden ml-3"} onClick={openMenu}>
             <Image
               src={isDarkMode ? assets.menu_white : assets.menu_black}
@@ -120,7 +107,7 @@ const NavBar = ({
 
         <ul
           ref={slideMenuRef}
-          className={`md:hidden flex flex-col fixed top-0 bottom-0 gap-4 w-64 -right-64 px-10 py-20 h-screen bg-gray-300 bg-opacity-90 transition duration-500 dark:bg-darkHover dark:text-white`}
+          className={`md:hidden text-lg flex flex-col fixed top-0 bottom-0 gap-16 w-64 -right-64 px-10 py-20 h-screen bg-[#fff5f5] bg-opacity-70 backdrop-blur-md transition duration-500 dark:bg-darkHover/70 dark:text-white`}
         >
           <div className={"absolute right-6 top-6"} onClick={closeMenu}>
             <Image
