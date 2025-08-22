484605a06e67855904a8ff30bd3746aa8124e004
calm-lee <calm_lee@naver.com>
2025-08-22 20:54:49 +0100

    fix: fix layout dark mode setting

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth dark">
+    <html lang="en" className="scroll-smooth">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
